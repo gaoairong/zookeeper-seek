2973987964e9f51a866101e83116019fc0cbcf9c
高振江 <407599698@qq.com>
2021-03-04 10:11:55 +0800

    init commit

---
+++ src/main/java/com/gac/zookeeper/zookeeperdemp/ZookeeperdempApplication.java
@@ -0,0 +1,13 @@
+package com.gac.zookeeper.zookeeperdemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperdempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZookeeperdempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gac/zookeeper/zookeeperdemp/ZookeeperdempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gac.zookeeper.zookeeperdemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZookeeperdempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
